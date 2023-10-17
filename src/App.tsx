e1600f2932c4b1d6efbc72dfc17924fe1a9fce03
vmdharan <vmdharan@users.noreply.github.com>
2023-10-17 18:11:22 +1100

    Created new component UIButton
    Created a new component called UIButton with three variants: primary, secondary and text.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import UIButton from './components/UIButton';
 
 const App = () => {
     return (
-        <>RPG Style UI</>
+        <>
+            <>RPG Style UI</>
+            <br />
+            <UIButton variant='primary' content='Primary' />
+            <UIButton variant='secondary' content='Secondary' />
+            <UIButton variant='text' content='Text' />
+        </>
     )
 };
 
